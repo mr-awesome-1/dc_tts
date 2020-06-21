2f8cf9ad4b32da6fc7fc81a40221d7b50c7b2767
Rakshit Shetty <callrmshetty@gmail.com>
2020-06-21 20:41:58 +0530

    Making it compatible for Colab

--- hyperparams.py
+++ hyperparams.py
@@ -32,7 +32,7 @@ class Hyperparams:
     attention_win_size = 3
 
     # data
-    data = "/data/private/voice/LJSpeech-1.0"
+    data = "/content/LJAudio"
     # data = "/data/private/voice/kate"
     test_data = 'harvard_sentences.txt'
     vocab = "PE abcdefghijklmnopqrstuvwxyz'.?" # P: Padding, E: EOS.
@@ -41,7 +41,7 @@ class Hyperparams:
 
     # training scheme
     lr = 0.001 # Initial learning rate.
-    logdir = "logdir/LJ01"
+    logdir = "/content/LJ_logdir/LJ01"
     sampledir = 'samples'
-    B = 32 # batch size
+    B = 16 # batch size
     num_iterations = 2000000
