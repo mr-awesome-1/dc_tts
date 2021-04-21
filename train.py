364da5c091fd166c00c4a7257ea83a190482a0c6
Anand Vishwkarma <vanand1999@gmail.com>
2021-04-21 18:02:29 +0530

    Update train.py

--- train.py
+++ train.py
@@ -142,13 +142,16 @@ if __name__ == '__main__':
 
     logdir = hp.logdir + "-" + str(num)
     sv = tf.train.Supervisor(logdir=logdir, save_model_secs=0, global_step=g.global_step)
+    count = 0
     with sv.managed_session() as sess:
         while 1:
+            if count == 2: break
             for _ in tqdm(range(g.num_batch), total=g.num_batch, ncols=70, leave=False, unit='b'):
                 gs, _ = sess.run([g.global_step, g.train_op])
 
                 # Write checkpoint files at every 1k steps
                 if gs % 1000 == 0:
+                    count += 1
                     sv.saver.save(sess, logdir + '/model_gs_{}'.format(str(gs // 1000).zfill(3) + "k"))
 
                     if num==1:
